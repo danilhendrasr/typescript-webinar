f76aca6561740d87d9200ee2fba83814175618d3
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-15 16:15:01 +0700

    feat(sesi-1): classes examples

--- sesi-1/5-classes.js
+++ sesi-1/5-classes.js
@@ -1,3 +1,29 @@
-class Person {
-    hat;
+class Human {
+    constructor(height, weight) {
+        this.height = height;
+        this.weight = weight;
+    }
 }
+
+const person = new Human(180, 72);
+console.log(person.height);
+
+class Worker extends Human {
+    #occupation;
+
+    constructor(height, weight, occupation) {
+        super(height, weight);
+        this.#occupation = occupation;
+    }
+
+    get occupation() {
+        return this.#occupation;
+    }
+
+    set occupation(newOccupation) {
+        this.#occupation = newOccupation;
+    }
+}
+
+const worker = new Worker(180, 72, "Engineer");
+console.log(worker.occupation);
