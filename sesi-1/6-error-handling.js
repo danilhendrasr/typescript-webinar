1b11f4fbb83bfaa615b983bef9a595e2d8aaf928
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-11-15 16:27:16 +0700

    feat(sesi-1): error handling

---
+++ sesi-1/6-error-handling.js
@@ -0,0 +1,21 @@
+try {
+    // const x = 10;
+    x / 5;
+} catch (e) {
+    console.log("Cannot do the above operations.");
+} finally {
+    console.log("This will always get executed.");
+}
+
+function createArray(length) {
+    try {
+        const newArray = new Array(length);
+        console.log("Array successfully created.");
+        return newArray;
+    } catch {
+        console.log(`Failed creating array with length of ${length}.`);
+    }
+}
+
+createArray(10);
+createArray(-1);
