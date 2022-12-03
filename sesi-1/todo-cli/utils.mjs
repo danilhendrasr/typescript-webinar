4a6d84d0c707d4d9f0d21c805457856d5643de5b
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2022-12-03 19:31:00 +0700

    feat(sesi-1): new todo-cli

--- sesi-1/todo-cli/utils.mjs
+++ sesi-1/todo-cli/utils.mjs
@@ -1,12 +1,9 @@
 import { readFileSync, writeFileSync } from "fs";
 
-// Read data from data.json
 export function readData() {
     return JSON.parse(readFileSync("./data.json"));
 }
 
-// Write to data.json
 export function writeData(data) {
-    const toWrite = JSON.stringify(data);
-    writeFileSync("./data.json", toWrite);
+    writeFileSync("./data.json", data);
 }
